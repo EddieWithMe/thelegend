81068e9101061ca9fb30b400e2c2fd221b6e5cbf
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2020-10-18 22:23:46 +0000

    Update AUTHORS file

--- GCE/exchange_classes/gdax/rest_ws_gdax.py
+++ GCE/exchange_classes/gdax/rest_ws_gdax.py
@@ -3,4 +3,28 @@
 #We throttle public endpoints by IP: 3 requests per second, up to 6 requests per second in bursts.
 
 #PRIVATE ENDPOINTS
-#We throttle private endpoints by user ID: 5 reques
+#We throttle private endpoints by user ID: 5 requests per second, up to 10 requests per second in bursts.
+
+"""
+    See https://docs.gdax.com/#ids
+"""
+import websocket
+from operator import itemgetter
+import hmac
+import hashlib
+import time
+import requests
+import threading
+import json
+from collections import deque
+#from firebase import set_in_firebase, set_child_value, firebase_read_value, firebase_push_value
+from security.secrets import TEST_GDAX_API_KEY, TEST_GDAX_API_SECRET, TEST_GDAX_API_PASSPHRASE
+import base64
+from requests.auth import AuthBase
+import sys
+import datetime
+import logging
+logging.basicConfig()
+
+BASE_URL = u'https://api.gdax.com'
+PUBLIC_CALLS = {u'products', u'product_orde
