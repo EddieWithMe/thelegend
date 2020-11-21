96c9f8a96a01b5de1fb5b245c128c6f81e86c158
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2020-11-21 21:54:02 +0000

    regmap: debugfs: Ensure we don't underflow when printing access masks

--- GCE/exchange_classes/gdax/rest_ws_gdax.py
+++ GCE/exchange_classes/gdax/rest_ws_gdax.py
@@ -381,4 +381,19 @@ class GDAXWebsocket(object):
 
 class GdaxAuth(AuthBase):
     # Provided by gdax: https://docs.gdax.com/#signing-a-message
-    de
+    def __init__(self, api_key, secret_key, passphrase):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.passphrase = passphrase
+
+    def __call__(self, request):
+        timestamp = str(time.time())
+        message = timestamp + request.method + request.path_url + (request.body or '')
+        message = message.encode('ascii')
+        hmac_key = base64.b64decode(self.secret_key)
+        signature = hmac.new(hmac_key, message, hashlib.sha256)
+        signature_b64 = base64.b64encode(signature.digest())
+        request.headers.update({
+            'Content-Type': 'Application/JSON',
+            'CB-ACCESS-SIGN': signature_b64,
+            'CB-ACCE
