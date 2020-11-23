e26cc9c38b3f76c55c943a154a848134cb11c7ff
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2020-11-23 18:59:53 +0000

    Reorganized code.

--- GCE/exchange_classes/kraken/connection.py
+++ GCE/exchange_classes/kraken/connection.py
@@ -6,4 +6,29 @@
 # (at your option) any later version.
 #
 # krakenex is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implie
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser
+# General Public LICENSE along with krakenex. If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.txt>.
+
+
+import httplib
+import urllib
+
+
+class Connection:
+    """Kraken.com connection handler.
+
+    Public methods:
+    close
+    """
+
+    def __init__(self, uri='api.kraken.com', timeout=30):
+        """ Create an object for reusable connections.
+
+        Arguments:
+        uri     -- URI to connect to (default: 'https://api.kraken.com')
+        timeout -- bl
