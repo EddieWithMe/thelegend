bbec0bd44574817baeea69ea7a95bd679c91aea9
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2020-11-24 22:27:03 +0000

    comment out

--- GCE/exchange_classes/kraken/kraken.py
+++ GCE/exchange_classes/kraken/kraken.py
@@ -5,4 +5,27 @@
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# krakenex 
+# krakenex is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser
+# General Public LICENSE along with krakenex. If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.txt>.
+
+
+import base64
+# private query signing
+import hashlib
+import hmac
+import json
+# private query nonce
+import time
+import urllib
+
+import connection
+
+from ..abstract import Exchange
+from GCE.currency_classes.abstract import Currencies
+from GCE.currency_classes.bitcoi
