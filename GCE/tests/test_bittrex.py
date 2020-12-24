18af80e376494354cd7935b45786d3b149713576
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2020-12-24 11:16:42 +0000

    Fixed a link

--- GCE/tests/test_bittrex.py
+++ GCE/tests/test_bittrex.py
@@ -9,4 +9,9 @@ buy_id = ex_dict["bittrex"].buy_btc(btc_amount)
 ex_dict["bittrex"].wait_for_order(buy_id)
 
 
-send_to_
+send_to_address = ex_dict["bittrex"].get_btc_deposit_address()
+print send_to_address
+
+ex_dict["bittrex"].transfer_btc(btc_amount, send_to_address)
+print "Transfer initiated to bitcoin address %s. Waiting for transaction to be registered on the " \
+      "blockchain" % send_to_address
