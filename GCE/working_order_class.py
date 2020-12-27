a9e44f31d8ee6003494dda6d72e9d6810673768e
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2020-12-27 13:47:28 +0000

    staging: comedi: cb_pcidas64: fix incorrect AI range code handling

--- GCE/working_order_class.py
+++ GCE/working_order_class.py
@@ -8,4 +8,21 @@ class WorkingOrder:
         self.send_to = wo_params_dict["send_to"].lower()
         self.buy_on = wo_params_dict["buy_on"].lower()
         self.required_spread = float(wo_params_dict["required_spread"])
-        self.tot
+        self.total_quantity = float(wo_params_dict["quantity"])
+        if self.total_quantity > 0:
+            self.buy_sell = "buy"
+        else:
+            self.buy_sell = "sell"
+        self.continue_loop = False
+        self.completed = False
+        self.working_order_id = 0
+        self.work_just_matched_price = 0
+        self.to_remove_count = 0
+        self.last_time = 0
+        self.counter = 0
+
+    def get_work_price(self, exchanges_container):
+        required_spread = self.required_spread
+        hedge_ask = exchanges_container.ex_dict[self.send_to].ask_price
+        hedge_bid = exchanges_container.ex_dict[self.send_to].bid_price
+ 
