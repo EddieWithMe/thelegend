be1cf487d8b87332a4fac303a22c5e1e85237db0
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2021-11-29 15:54:35 +0000

    nudge

---
+++ gdax_websocket.py
@@ -0,0 +1,13 @@
+import gdax, time
+
+
+class myWebsocketClient(gdax.WebsocketClient):
+    def on_open(self):
+        self.url = "wss://ws-feed.gdax.com/"
+        self.products = ["BTC-USD"]
+        self.message_count = 0
+        print("Lets count the messages!")
+    def on_message(self, msg):
+        self.message_count += 1
+        if 'price' in msg and 'type' in msg:
+       
