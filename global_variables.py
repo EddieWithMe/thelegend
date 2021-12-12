9d7ff273a1f7ab2c108577d3de6265f29c95164f
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2021-12-12 18:53:28 +0000

    implemented review comments

--- global_variables.py
+++ global_variables.py
@@ -4,4 +4,14 @@ WINDOW_IN_SECONDS = 1800
 SIGN_UP_FLOW_EMAIL_ADDRESS_QA = "signup@coin-temple.appspotmail.com"
 SIGN_UP_FLOW_EMAIL_ADDRESS_PROD = "signup@coin-temple.appspotmail.com"
 SUPPORT_EMAIL_ADDRESS_QA = "support@coin-temple.appspotmail.com"
-SUPPORT_EMAIL_A
+SUPPORT_EMAIL_ADDRESS_PROD = "support@coin-temple.appspotmail.com"
+SERVICE_NAME = "Coin Temple"
+LENGTH_OF_UUID4 = 36
+ABOUT_PAGE_URL = "http://coin-temple.appspotmail.com"
+
+MAX_BEFORE_SIGNUP_CAPTCHA = 3
+MAX_SIGNUP_REQUESTS_BEFORE_TEMP_BAN = 7
+MAX_BEFORE_LOGIN_CAPTCHA = 7
+MAX_LOGIN_ATTEMPTS_BEFORE_TEMP_BAN = 20
+WINDOW_IN_SECONDS = 1800
+MAX_TEMP_BAN_BEFORE_PERMANENT = 3
