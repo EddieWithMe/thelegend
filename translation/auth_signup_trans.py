62171483571873133e2313948f1484a76cf34912
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2023-05-27 19:11:46 +0000

    debug msg

--- translation/auth_signup_trans.py
+++ translation/auth_signup_trans.py
@@ -19,4 +19,24 @@ class auth_signup_en:
     en17 = u"FORGOT PASSWORD"
 
     def __init__(self):
-        self.dat
+        self.data=[]
+
+class auth_signup_es:
+    en0 = u"Inscribirse"
+    en1 = u"Introduzca su Email, Nombre/Apellido, Número de Teléfono, y Contraseña."
+    en2 = u"Weak"
+    en3 = u"EMAIL"
+    en4 = u"PRIMER NOMBRE"
+    en5 = u"APELLIDO"
+    en6 = u"NÚMERO DE TELÉFONO"
+    en7 = u"- used for fraud protection (two factor authentication)"
+    en8 = u"CONTRASEÑA"
+    en9 = u"REPITA CONTRASEÑA"
+    en10 = u"By signing up I agree to the "
+    en11 = u"Terms of Use"
+    en12 = u"and"
+    en13 = u"Privacy"
+    en14 = u"SOMETER"
+    en15 = u"- only used for transactional (non-spam) communication"
+    en16 = u"Security Policy"
+    en17 = u"CONTRASEÑA 
