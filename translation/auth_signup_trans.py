4f8ee998ae1929a070f92f0f2ac995b8b599510c
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2023-05-27 14:56:14 +0000

    3.1.5

---
+++ translation/auth_signup_trans.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+
+class auth_signup_en:
+    en0 = u"Sign Up"
+
+    en1 = u"Enter your Email, First/Last Name, Phone Number, and Password."
+    en2 = u"Weak"
+    en3 = u"EMAIL"
+    en4 = u"USERNAME"
+    en8 = u"PASSWORD"
+    en9 = u"REPEAT PASSWORD"
+    en10 = u"By signing up I agree to the "
+    en11 = u"Terms of Use"
+    en12 = u"and"
+    en13 = u"Privacy"
+    en14 = u"SUBMIT"
+    en16 = u"Security"
+    en17 = u"FORGOT PASSWORD"
+
+    def __init__(self):
+        self.dat
