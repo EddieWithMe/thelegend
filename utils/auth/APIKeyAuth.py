793d109b2ae35458e86815e5d46cd787e14d67a0
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2023-06-01 15:53:34 +0000

    Log changes.

--- utils/auth/APIKeyAuth.py
+++ utils/auth/APIKeyAuth.py
@@ -19,4 +19,20 @@ class APIKeyAuth(AuthBase):
         self.apiSecret = apiSecret
 
     def __call__(self, r):
-        """Called whe
+        """Called when forming a request - generates api key headers."""
+        # modify and return the request
+        nonce = generate_nonce()
+        r.headers['api-nonce'] = str(nonce)
+        r.headers['api-key'] = self.apiKey
+        r.headers['api-signature'] = generate_signature(self.apiSecret, r.method, r.url, nonce, r.body or '')
+        return r
+
+
+def generate_nonce():
+    return int(round(time.time() * 1000))
+
+
+# Generates an API signature.
+# A signature is HMAC_SHA256(secret, verb + path + nonce + data), hex encoded.
+# Verb must be uppercased, url is relative, nonce must be an increasing 64-bit integer
+# and the data, if present, 
