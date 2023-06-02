3a828908c758f92559dd5e5ae8dea0e64d3b8805
EddieWithMe <92177990+EddieWithMe@users.noreply.github.com>
2023-06-02 14:32:05 +0000

    coquille

--- utils/auth/APIKeyAuthWithExpires.py
+++ utils/auth/APIKeyAuthWithExpires.py
@@ -11,4 +11,17 @@ class APIKeyAuthWithExpires(AuthBase):
     def __init__(self, apiKey, apiSecret):
         """Init with Key & Secret."""
         self.apiKey = apiKey
-    
+        self.apiSecret = apiSecret
+
+    def __call__(self, r):
+        """
+        Called when forming a request - generates api key headers. This call uses `expires` instead of nonce.
+
+        This way it will not collide with other processes using the same API Key if requests arrive out of order.
+        For more details, see https://www.bitmex.com/app/apiKeys
+        """
+        # modify and return the request
+        expires = int(round(time.time()) + 5)  # 5s grace period in case of clock skew
+        r.headers['api-expires'] = str(expires)
+        r.headers['api-key'] = self.apiKey
+        r.headers['api-signature'] = generate_signature(self.apiSecret, r.
